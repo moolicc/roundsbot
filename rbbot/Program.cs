e617de3c2ebd90be44a3d6f34d865b15716ae6e3
Icecream-burglar <>
2017-09-11 15:26:57 -0500

    Add project files.

---
+++ rbbot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rbbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
