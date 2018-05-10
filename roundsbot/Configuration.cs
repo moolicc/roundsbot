f618112e8bb46e2e532f2f3c3c4ad0d9dcf7fdb6
Icecreamburglar <icecreamburglar@gmail.com>
2018-05-10 15:11:53 -0500

    Config cleanup

--- roundsbot/Configuration.cs
+++ roundsbot/Configuration.cs
@@ -1,29 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace roundsbot
 {
     class Configuration
     {
+        public string Token { get; set; }
         public ulong ChannelId { get; set; }
-
+        
         public int RoundLength { get; set; }
         public int BreakLength { get; set; }
-
-        public int CountdownStart { get; set; }
         public int TimeoutCount { get; set; }
         
+        [JsonIgnore]
         public DateTime LaunchTime { get; private set; }
 
         public Configuration()
         {
+            Token = "";
             ChannelId = 0;
+
             RoundLength = 25;
             BreakLength = 5;
-            CountdownStart = 180;
-            TimeoutCount = 3;
-            LaunchTime = DateTime.Now;
+            TimeoutCount = 5;
+
+            LaunchTime = DateTime.UtcNow;
         }
     }
 }
