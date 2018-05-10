2bfabef8537bc4eb51e80a15cd9e725c88fb81fa
Icecreamburglar <icecreamburglar@gmail.com>
2018-05-10 15:09:37 -0500

    Cleanup

--- roundsbot/Configuration.cs
+++ roundsbot/Configuration.cs
@@ -6,19 +6,15 @@ namespace roundsbot
 {
     class Configuration
     {
-
         public ulong ChannelId { get; set; }
+
         public int RoundLength { get; set; }
         public int BreakLength { get; set; }
+
         public int CountdownStart { get; set; }
         public int TimeoutCount { get; set; }
-
-        public List<ulong> SubscribedUsers { get; private set; }
+        
         public DateTime LaunchTime { get; private set; }
-
-        public Version Version { get; private set; }
-        public string[] Authors { get; private set; }
-        public DateTime ReleaseDate { get; private set; }
 
         public Configuration()
         {
@@ -27,19 +23,7 @@ namespace roundsbot
             BreakLength = 5;
             CountdownStart = 180;
             TimeoutCount = 3;
-            SubscribedUsers = new List<ulong>();
             LaunchTime = DateTime.Now;
-
-            LoadAbout();
-        }
-
-        private void LoadAbout()
-        {
-            var data = System.IO.File.ReadAllLines("about.txt");
-            Version = new Version(data[0]);
-            Authors = new string[data.Length - 1];
-            Array.Copy(data, 1, Authors, 0, Authors.Length);
-            ReleaseDate = new DateTime(2017, 9, 25, 23, 25, 0, DateTimeKind.Utc);
         }
     }
 }
