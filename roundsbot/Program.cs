3e9839c97ee7fd06de4df10dbf0e0e4634057bcb
Icecreamburglar <icecreamburglar@gmail.com>
2018-05-10 15:25:41 -0500

    Program.cs rewritten

--- roundsbot/Program.cs
+++ roundsbot/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Net.WebSocket;
+using Newtonsoft.Json;
 
 namespace roundsbot
 {
@@ -12,6 +13,27 @@ namespace roundsbot
     {
         static void Main(string[] args)
         {
+            if (!File.Exists("conf.json"))
+            {
+                var conf = new Configuration();
+                File.WriteAllText("conf.json", JsonConvert.SerializeObject(conf));
+
+                Console.WriteLine("Please edit the conf.json file and add the bot's token. Then restart the bot.");
+                Console.ReadLine();
+                return;
+            }
+
+            var config = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText("conf.json"));
+            var discord = new Discord();
+            discord.Connect(config);
+
+            //TODO: Create REPL (and a command to save the config)
+            while (Console.ReadLine() != "exit")
+            {
+                
+            }
+            
+            File.WriteAllText("conf.json", JsonConvert.SerializeObject(discord.DiscordConfig));
         }
     }
 }
